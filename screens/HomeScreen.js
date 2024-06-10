03355348c2262307432f01e044f0cb12155fcb73
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2024-06-10 12:45:26 +0530

    package changes

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,11 @@
 import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import moment from 'moment'
 
 const HomeScreen = () => {
+ const currentDate = moment();
+ const startOfWeek = currentDate.clone().startOf('week');
+ const [date, setDate] = useState("")
  const renderWeeks = numWeeks => {
     let weeks = []
     for(let i=0; i<numWeeks; i++){
@@ -17,7 +21,8 @@ const HomeScreen = () => {
   return (
     <ScrollView >
       <View style={{flex: 1 , padding: 12}}>
-        {renderWeeks}
+        {/* {renderWeeks} */}
+        <Text>Hello</Text>
       </View>
     </ScrollView>
   )
